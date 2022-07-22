0d397c3854d279bfa4d7bd6416032a3a8a967ba3
ammyy9908 <sb78639@gmail.com>
2022-07-22 14:38:13 +0530

    Admin added

---
+++ admin/login/js/index.js
@@ -0,0 +1,32 @@
+// make a login request to server
+const adminForm = document.getElementById("admin-form");
+
+async function Login(email, password) {
+  console.log(email, password);
+  try {
+    const r = await axios.post(
+      `https://employee-system313.herokuapp.com/admin/login`,
+      {
+        email,
+        password,
+      }
+    );
+    console.log(r.data);
+    return r.data;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+adminForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  let token = await Login(email, password);
+  if (token) {
+    Cookies.set("token", token);
+    Cookies.set("role", "admin");
+    window.location.href = "/";
+  }
+});
---
+++ profile/u/js/index.js
@@ -0,0 +1,58 @@
+async function fetchUserInfo() {
+  console.log("Run");
+  try {
+    const r = await axios.get(
+      `https://employee-system313.herokuapp.com/auth/user`,
+      {
+        headers: {
+          Authorization: `${Cookies.get("token")}`,
+        },
+      }
+    );
+    return r.data;
+  } catch (e) {
+    if (e.response && e.response.data) {
+      console.log(e.response.data);
+      return false;
+    }
+  }
+}
+
+async function fetchAdminInfo() {
+  console.log("Run");
+  try {
+    const r = await axios.get(`http://localhost:5000/admin/me`, {
+      headers: {
+        Authorization: `${Cookies.get("token")}`,
+      },
+    });
+    return r.data;
+  } catch (e) {
+    if (e.response && e.response.data) {
+      console.log(e.response.data);
+      return false;
+    }
+  }
+}
+
+async function Init() {
+  if (Cookies.get("role") === "admin") {
+    const data = await fetchAdminInfo();
+    console.log(data);
+    const { name, email } = data;
+    document.querySelector(".name").textContent = name;
+    document.querySelector(".email").textContent = email;
+    document.querySelector(".user__avatar").style.display = "none";
+  } else {
+    const data = await fetchUserInfo();
+    console.log(data);
+    const { name, email, avatar } = data;
+    document.querySelector(".name").textContent = name;
+    document.querySelector(".email").textContent = email;
+    document.querySelector(
+      ".user__avatar"
+    ).style.backgroundImage = `url(${avatar})`;
+  }
+}
+
+Init();
